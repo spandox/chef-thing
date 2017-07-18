f54d3d3dfb096c608c6408d7021f6e61c65f3ff3
Michael Sandler <spandox@spandox.com>
2017-07-18 16:30:06 +0000

    postgresql cookbook

--- cookbooks/apache/recipes/motd.rb
+++ cookbooks/apache/recipes/motd.rb
@@ -1,5 +1,24 @@
 hostname = node['hostname']
-file '/etc/motd' do
-	content "Hostname is this: #{hostname}"
+package 'fortune-mod' do
+	action :install
+end 
+
+execute 'command-test0' do
+        command 'echo > /etc/motd'
+        only_if { ::File.exists?('/etc/motd') }
 end
 
+execute 'command-test1' do
+        command 'fortune >> /etc/motd'
+        only_if { ::File.exists?('/etc/motd') }
+end
+
+execute 'command-test2' do
+	command 'echo -e \\\n >> /etc/motd'
+        only_if { ::File.exists?('/etc/motd') }
+end
+
+execute 'command-test3' do
+	command "echo Hostname: #{hostname} >> /etc/motd"
+        only_if { ::File.exists?('/etc/motd') }
+end
